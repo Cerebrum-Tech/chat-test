605f3fa914db6af6063544b8eb5a2ba3b7c439a1
Alp Eren Özalp <alperenoz93@gmail.com>
2026-04-05 15:49:52 +0300

    chore(01-migration-01): replace Chatwoot env vars with Waters Bot vars and trim WebView types
    - env.sample: replace 5 Chatwoot vars with 4 Waters Bot vars (BOT_URL, USER_ID, ACCESS_TOKEN, LANGUAGE)
    - types/webview.ts: remove WebViewMessageHandler interface (D-07), keep WebViewMessage, GotoPageMessage, ChatWebViewProps

--- types/webview.ts
+++ types/webview.ts
@@ -11,13 +11,8 @@ export interface GotoPageMessage extends WebViewMessage {
   };
 }
 
-export interface WebViewMessageHandler {
-  onGotoPage: (data: GotoPageMessage['Data']) => void;
-  onUnknownMessage: (message: WebViewMessage) => void;
-}
-
 export interface ChatWebViewProps {
   onMessage?: (message: WebViewMessage) => void;
   onNavigationRequest?: (pageName: string, caseId: string) => void;
   style?: any;
-} 
+}
